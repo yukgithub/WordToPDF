0099f3864ee8d96215aa9a735a20dea146aa9c8f
yuki <mekata@mgial.com>
2020-11-30 22:50:22 +0900

    First commit

---
+++ WordToPDF/Main.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WordToPDF
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolComplete = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(467, 109);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(100, 29);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "変換";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(99, 14);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(468, 22);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "T:\\";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(99, 51);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(4);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(468, 22);
+            this.txtOutput.TabIndex = 2;
+            this.txtOutput.Text = "S:\\";
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(575, 51);
+            this.btnOutput.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(35, 22);
+            this.btnOutput.TabIndex = 3;
+            this.btnOutput.Text = "...";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(575, 14);
+            this.btnInput.Margin = new System.Windows.Forms.Padding(4);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(35, 22);
+            this.btnInput.TabIndex = 1;
+            this.btnInput.Text = "...";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "入力フォルダ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "出力フォルダ";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolComplete});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 154);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(633, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolComplete
+            // 
+            this.toolComplete.Name = "toolComplete";
+            this.toolComplete.Size = new System.Drawing.Size(0, 17);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 176);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnConvert);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "Convert Word File to PDF ";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolComplete;
+    }
+}
+
---
+++ WordToPDF/Main.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using RefWord = Microsoft.Office.Interop.Word;
+
+namespace WordToPDF
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 入力ボタンクリック処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "フォルダを選択";
+
+                if(dialog.ShowDialog(this) == DialogResult.OK)
+                {
+                    this.txtInput.Text = dialog.SelectedPath;
+                    this.txtOutput.Text = dialog.SelectedPath;
+                }
+
+
+            }
+        }
+
+        /// <summary>
+        /// 出力ボタンクリック処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "フォルダを選択";
+
+                if (dialog.ShowDialog(this) == DialogResult.OK)
+                    this.txtOutput.Text = dialog.SelectedPath;
+
+
+            }
+
+        }
+
+        /// <summary>
+        /// 変換ボタンクリック処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+
+            RefWord.Application word = new RefWord.Application();
+            RefWord.Documents docs = word.Documents;
+            RefWord.Document doc = null;
+
+            this.toolComplete.Text = string.Empty;
+            this.Cursor = Cursors.WaitCursor;
+
+            try
+            {
+
+                string[] files = Directory.GetFiles(this.txtInput.Text, "*.doc*");
+
+                foreach (string f in files)
+                {
+                    this.toolComplete.Text = Path.GetFileName(f);
+
+                    var attribute = File.GetAttributes(f);
+                    var fn = Path.GetFileNameWithoutExtension(f);
+
+                    //隠しファイル（テンポラリ）は飛ばす
+                    if ((attribute & FileAttributes.Hidden) == FileAttributes.Hidden)
+                        continue;
+
+                    //出力
+                    doc = docs.Open(f, ReadOnly:true);
+                    doc.ExportAsFixedFormat(this.txtOutput.Text + "\\" + fn + ".pdf", RefWord.WdExportFormat.wdExportFormatPDF);
+                    doc.Close();
+
+                }
+
+                this.toolComplete.Text = "完了";
+
+            } catch(Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            } finally
+            {
+
+                MessageBox.Show(GC.GetTotalMemory(false).ToString());
+
+                
+                //GC.Collect();
+               // GC.WaitForPendingFinalizers();
+                //GC.Collect();
+                
+
+                if (word != null) word.Quit();
+
+                Marshal.ReleaseComObject(doc);
+                Marshal.ReleaseComObject(docs);
+                Marshal.ReleaseComObject(word);
+
+                
+                //GC.Collect();
+                //GC.WaitForPendingFinalizers();
+                //GC.Collect();
+                
+
+                MessageBox.Show(GC.GetTotalMemory(false).ToString());
+
+            }
+
+            this.Cursor = Cursors.Default;
+
+        }
+
+        /// <summary>
+        /// 起動時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.txtInput.Text = Directory.GetCurrentDirectory();
+            this.txtOutput.Text = Directory.GetCurrentDirectory();
+        }
+    }
+}
